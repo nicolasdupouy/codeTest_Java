99e4dd01a898fc0ef935a987d8bded1030a3496f
Nicolas Dupouy <nicolasdupouy@gmail.com>
2020-05-21 20:36:51 +0200

    Add a first feature

---
+++ hellocucumber/README.md
@@ -0,0 +1,11 @@
+# Cucumber exemple
+
+## Launch the Cucumber Gradle task
+
+### with the binary installed on the OS
+
+    $ gradle cucumber
+    
+### with the gradle wrapper bundled in the project
+
+    $ ./gradlew cucumber
---
+++ hellocucumber/src/test/java/hellocucumber/Stepdefs.java
@@ -0,0 +1,26 @@
+package hellocucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class Stepdefs {
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Sunday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        Assert.assertEquals(expectedAnswer, actualAnswer);
+    }
+}
